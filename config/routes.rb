911ea41548d1cf1d79a9b8f5f4c2cbbaa3d034b1
Christian de Courcy <chris@hyper.no>
2018-04-04 22:15:12 +0200

    bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,6 @@ Rails.application.routes.draw do
     resources :sessions, only: [:index, :create, :destroy]
     resources :moderators, only: [:index, :edit, :update]
     resources :uploads, only: [:create, :destroy]
-    resources :documents, only: [:destroy]
 
   end
 
